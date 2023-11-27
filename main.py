d0b89eca3f1c28709dd3827c48f8158f5f972a76
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-27 22:17:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,36 @@ import os
 def format_message(content: str, category: str) -> str:
     return f"🌟 *{category}*: {content} 🌟"
 
-def process_command(update: Update, context: CallbackContext, content_list: list, category: str) -> None:
-    args = context.args
-    if not args:
-        update.message.reply_text(f"Please use the command in the format `/{category} <number>`.")
+def process_command(update: Update, context: CallbackContext) -> None:
+    command_args = context.args
+    if not command_args:
+        update.message.reply_text("Please provide the command in the format `/<category> <number>`.")
         return
 
+    category = command_args[0].lower()
+    content_list = None
+
+    if category == 'sspam':
+        content_list = shayari_list
+    elif category == 'joke':
+        content_list = jokes_list
+    elif category == 'gana':
+        content_list = songs_lyrics
+    elif category == 'mspam':
+        content_list = love_shayari
+    elif category in ['dialogue', 'dialogues']:
+        content_list = dialogue_list
+
+    if content_list:
+        process_command_for_category(update, context, content_list, category)
+    else:
+        update.message.reply_text("Invalid category. Supported categories are: sspam, joke, gana, mspam, dialogue(s).")
+
+def process_command_for_category(update: Update, context: CallbackContext, content_list: list, category: str) -> None:
+    command_args = context.args[1:]  # Exclude the category from the arguments
     try:
-        num_items = int(args[0])
-    except ValueError:
+        num_items = int(command_args[0])
+    except (ValueError, IndexError):
         update.message.reply_text("Please enter a valid number.")
         return
 
@@ -33,7 +54,7 @@ def process_command(update: Update, context: CallbackContext, content_list: list
     else:
         selected_items = random.sample(content_list, num_items)
 
-    formatted_messages = [format_message(item, category) for item in selected_items]
+    formatted_messages = [format_message(item, category.capitalize()) for item in selected_items]
     for formatted_message in formatted_messages:
         update.message.reply_text(formatted_message, parse_mode=ParseMode.MARKDOWN)
 
@@ -42,31 +63,25 @@ def stop_command(update: Update, context: CallbackContext) -> None:
 
 def start(update: Update, context: CallbackContext) -> None:
     if update.message.chat.type == 'private':
-        # Handle start command in private messages differently
         update.message.reply_text("Welcome! You can use commands like /sspam, /joke, /gana, /mspam, /dialogue, and /dialogues.")
     else:
-        # Check if the user is a group administrator
         if not update.message.chat.get_member(update.message.from_user.id).status in ['administrator', 'creator']:
             update.message.reply_text("Only group administrators can start the bot in this group.")
             return
 
-        # Bot logic for group start command
-        # ...
-
 def main() -> None:
-    updater = Updater(os.environ.get("BOT_TOKEN"))  # BOT_TOKEN is set in the Heroku Config Vars
+    updater = Updater(os.environ.get("BOT_TOKEN"))
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("sspam", process_command, pass_args=True, callback_args=[shayari_list, "Shayari"]))
-    dp.add_handler(CommandHandler("joke", process_command, pass_args=True, callback_args=[jokes_list, "Joke"]))
-    dp.add_handler(CommandHandler("gana", process_command, pass_args=True, callback_args=[songs_lyrics, "Song"]))
-    dp.add_handler(CommandHandler("mspam", process_command, pass_args=True, callback_args=[love_shayari, "Love Shayari"]))
-    dp.add_handler(CommandHandler("dialogue", process_command, pass_args=True, callback_args=[dialogue_list, "Dialogue"]))
-    dp.add_handler(CommandHandler("dialogues", process_command, pass_args=True, callback_args=[dialogue_list, "Dialogue"]))
+    dp.add_handler(CommandHandler("sspam", process_command, pass_args=True))
+    dp.add_handler(CommandHandler("joke", process_command, pass_args=True))
+    dp.add_handler(CommandHandler("gana", process_command, pass_args=True))
+    dp.add_handler(CommandHandler("mspam", process_command, pass_args=True))
+    dp.add_handler(CommandHandler("dialogue", process_command, pass_args=True))
+    dp.add_handler(CommandHandler("dialogues", process_command, pass_args=True))
     dp.add_handler(CommandHandler("stop", stop_command))
 
-    # Handle private messages to start the bot
     dp.add_handler(MessageHandler(Filters.chat_type.private & ~Filters.command, start))
 
     updater.start_polling()
